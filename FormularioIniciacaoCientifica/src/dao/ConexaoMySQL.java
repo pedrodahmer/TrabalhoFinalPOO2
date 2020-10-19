8dea1be6e4b956891b7c4451c0d5d0b203a77fd4
Rodrigo Duarte Fonseca <rodrigoduarte31@gmail.com>
2020-10-19 12:12:43 -0300

    Add class ConexaoMySQL

--- FormularioIniciacaoCientifica/src/dao/ConexaoMySQL.java
+++ FormularioIniciacaoCientifica/src/dao/ConexaoMySQL.java
@@ -1,5 +1,66 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import dao.ConexaoMySQL;
+
 public class ConexaoMySQL {
+	
+	private static Connection con;
+	private static String status;
+	
+	public ConexaoMySQL() {
+		
+	}
+	
+	public static void abrirConexao() {
+		
+		String serverName = "localhost";
+		String database = "meubanco";
+		String url = "jdbc:mysql://" + serverName + ":3306/" + database + "?useTimezone=true&serverTimezone=UTC";
+		String userName = "rodrigo"; // Alterar usuario do BD aqui
+		String password = "root"; // Alterar senha aqui
+		
+		try {
+			con = DriverManager.getConnection(url, userName, password);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
+		if(con != null) {
+			status = "STATUS -----> Conectado com sucesso!";
+		} else {
+			status = "STATUS -----> Não foi possível realizar a conexão";
+		}
+		
+	}
+	
+	public static boolean fecharConexao() {
+		
+		try {
+			con.close();
+			System.out.println("Conexão encerrada!");
+			return true;
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			return false;
+		}
+
+	}
+	
+	public static void obterStatusConexao() {
+		System.out.println(status);
+	}
+	
+	public static void main(String[] args) {
+		
+		ConexaoMySQL.abrirConexao();
+		ConexaoMySQL.obterStatusConexao();
+		ConexaoMySQL.fecharConexao();
+		
+	}
 
 }
